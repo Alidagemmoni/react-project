a557a8d5222063f5932ce9c0e1740f1a538e5ecf
Alidagemmoni <45143677+Alidagemmoni@users.noreply.github.com>
2020-05-15 13:55:33 +0200

    initial commit

---
+++ src/constants/api.js
@@ -0,0 +1,8 @@
+const KEY = "5ea81ba93851a633426eb826";
+const BASE_URL = "https://us-central1-noroff-final-exam.cloudfunctions.net/api/v1/";
+const FETCH_OPTIONS = {
+    headers: {
+        "Content-Type": "application/json",
+        key: KEY,
+    },
+};
